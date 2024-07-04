57e8e87590231764fcb30fd2fb11137a3105af8c
AAK <kamyana786138@gmail.com>
2024-07-04 10:30:38 -0700

    AdminDashboardActivity UserMAnagementActivity

--- app/src/main/java/com/faa/cmsportalcui/AdminDashboardActivity.kt
+++ app/src/main/java/com/faa/cmsportalcui/AdminDashboardActivity.kt
@@ -8,14 +8,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
-    private lateinit var googleBtn: Button
+class AdminDashboardActivity : AppCompatActivity() {
+    private lateinit var adduserBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        googleBtn = findViewById(R.id.btn_google)
-        googleBtn.setOnClickListener {
-            startActivity(Intent(this@LoginActivity, AdminDashboardActivity::class.java))
+        setContentView(R.layout.activity_admin_dashboard)
+        adduserBtn = findViewById(R.id.btn_add_user)
+
+        adduserBtn.setOnClickListener {
+            startActivity(Intent(this@AdminDashboardActivity, UserMAnagementActivity::class.java))
         }
 
     }
