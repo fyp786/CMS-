426053b8ed4d676e9b3313dbcfe34a3270455eba
AAK <kamyana786138@gmail.com>
2024-07-06 07:04:25 -0700

    StaffListActivity

--- app/src/main/java/com/faa/cmsportalcui/Admin/NotificationActivity.kt
+++ app/src/main/java/com/faa/cmsportalcui/Admin/NotificationActivity.kt
@@ -1,14 +1,24 @@
 package com.faa.cmsportalcui.Admin
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.faa.cmsportalcui.R
 
 class NotificationActivity : AppCompatActivity() {
+    private lateinit var notificationBtn: Button
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notification)
+        notificationBtn = findViewById(R.id.notification_button)
+
+        notificationBtn.setOnClickListener {
+            startActivity(Intent(this@NotificationActivity, StaffListActivity::class.java))
+        }
 
     }
 }
