89238860abde31404a02554f2f5da40f347f06e0
AAK <kamyana786138@gmail.com>
2024-07-30 23:26:04 -0700

    Setting Acitivities

---
+++ app/src/main/java/com/faa/cmsportalcui/Admin/AdminGeneralSettingActivity.kt
@@ -0,0 +1,16 @@
+package com.faa.cmsportalcui.Admin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.faa.cmsportalcui.R
+
+class AdminGeneralSettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_general_setting)
+
+    }
+}
---
+++ app/src/main/java/com/faa/cmsportalcui/Admin/CompleteTaskActivity.kt
@@ -0,0 +1,15 @@
+package com.faa.cmsportalcui.Admin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.faa.cmsportalcui.R
+
+class CompleteTaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_complete_task)
+    }
+}
