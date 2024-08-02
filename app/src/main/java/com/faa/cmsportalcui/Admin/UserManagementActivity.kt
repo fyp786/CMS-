326f235ba9b61a520d880c37c7d7c6d46aa18d1f
AAK <kamyana786138@gmail.com>
2024-08-02 11:59:53 -0700

    User

--- app/src/main/java/com/faa/cmsportalcui/Admin/UserManagementActivity.kt
+++ app/src/main/java/com/faa/cmsportalcui/Admin/UserManagementActivity.kt
@@ -43,7 +43,6 @@ class UserManagementActivity : AppCompatActivity() {
                 userAdapter.notifyDataSetChanged()
             }
             .addOnFailureListener { exception ->
-                // Handle error
             }
     }
 }
